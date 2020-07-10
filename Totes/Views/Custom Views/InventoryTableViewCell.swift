539f3584579c7ab9acf3c062a3f5a13fb6bc8155
Moses <gabe.susman@gmail.com>
2020-07-09 22:16:20 -0600

    added trash and trash section table view controllers so that users can retrieve old items and sections the have deleted, also decided to go with custom table view cells to better implement the trash functionality, I also added cust table view cell files for every tableview controller with the exception of transactions(4 total)

---
+++ Totes/Views/Custom Views/InventoryTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  InventoryTableViewCell.swift
+//  Totes
+//
+//  Created by Moses on 7/9/20.
+//  Copyright © 2020 Max Susman. All rights reserved.
+//
+
+import UIKit
+
+class InventoryTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}

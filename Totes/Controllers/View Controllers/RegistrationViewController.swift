815738b2630ded0b332df22e4496302ffb005f9e
Moses <gabe.susman@gmail.com>
2020-07-27 06:17:30 -0600

    added registration functionality to the RegistrationViewController and created the Email Verification View Controller to send the user to with a label that informs them that they need to verify their account creation from their email

--- Totes/Controllers/View Controllers/RegistrationViewController.swift
+++ Totes/Controllers/View Controllers/RegistrationViewController.swift
@@ -26,21 +26,34 @@ class RegistrationViewController: UIViewController {
     }
     
     // MARK: - Actions
+    // I need to add the logic to create a user in the firestore db using this action
     @IBAction func registerButton(_ sender: Any) {
+        // casts email username and password text field optionals as strings and ensures they meet reqs
+        // what is best practice for determining a valid email?
+        // we also need to query the db and ensure accounts with this username and or email don't already exist on record
+        guard let email = emailAddressEntryTextField.text as? String, email != "", let username = usernameEntryTextField.text as? String, username.count >= 8, let password = passwordEntryTextField.text as? String, password.count >= 8 else { return }
+        
+        print(email)
+        print(username)
+        print(password)
+        // we need visual feedback for when the user enters the wrong email
     }
+    // fine as i
     @IBAction func forgottenPasswordButton(_ sender: Any) {
     }
     
 
+    // we need to do a conditional segue to the email verification view controller
 
-    /*
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
+    // "Framework not found GoogleAppMeasurement"
+//    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+//        if let vc = segue.destination as? EmailVerificationViewController
+//        {
+//
+//        }
+//    }
 
 }
